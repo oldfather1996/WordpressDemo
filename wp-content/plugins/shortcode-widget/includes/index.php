ed21a455d88acfc37b950d1745bf34bf60fc7cb2
knight <oldfather1996@gmail.com>
2021-10-18 00:04:04 +0700

    demo

--- wp-content/plugins/shortcode-widget/includes/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Silence is golden
- *
- * This file exists to stop directory listings on poorly configured servers.
- */
--- wp-content/themes/theme-base/templates/header/language.php
+++
@@ -1,6 +0,0 @@
-<?php
-    if (class_exists('SitePress')) {
-        Themebase_WPML::show_language_dropdown();
-    }else{
-        Themebase_WPML::show_language_dropdown_demo();
-    }
